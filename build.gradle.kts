35f7d6f544252cd11b73fbc1a93ea06e6c9f11b9
peti <petikoch@gmail.com>
2019-05-22 15:28:05 +0200

    - Gradle 5.4.1 with Gradle Kotlin Script - Spring Boot 2.1.x - Vaadin 13.0.x - Kotlin 1.3.x

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.1.5.RELEASE"
+	id("io.spring.dependency-management") version "1.0.7.RELEASE"
+	kotlin("jvm") version "1.3.31"
+	kotlin("plugin.spring") version "1.3.31"
+	id("com.github.ben-manes.versions") version "0.21.0"
+}
+
+group = "ch.petikoch.examples"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+}
+
+extra["vaadinVersion"] = "13.0.7"
+
+dependencies {
+	implementation("com.vaadin:vaadin-spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
---
+++ src/main/kotlin/ch/petikoch/examples/gaesbvkge/ui/vaadin/MainView.kt
@@ -0,0 +1,15 @@
+package ch.petikoch.examples.gaesbvkge.ui.vaadin
+
+import com.vaadin.flow.component.Text
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.router.Route
+
+@Route
+class MainView : VerticalLayout() {
+    init {
+        add(Text("Hello World!"))
+        add(Button("Click me") { e -> Notification.show("Hello Google AppEngine  Spring+Vaadin user!") })
+    }
+}
